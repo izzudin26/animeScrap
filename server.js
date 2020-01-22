b0771688cf93625bdcc4814c9fc8227db1c6045a
Azidarmor26 <azidarmor26@gmail.com>
2020-01-22 19:55:11 +0700

    update cors for api

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ const router = express.Router()
 const port = process.env.PORT || 3000
 const app = express()
 const bodyparser = require('body-parser')
+const cors = require('cors')
 
 
 app
     .use(bodyparser.json())
+    .use(cors())
     .listen(port, () => {
         console.log("Running on port server http://localhost:" + port)
     })
